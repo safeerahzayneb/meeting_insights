cc1788463f739908b061b78b932d71e2fb837c1e
Jennifer Chen <jenn.chenn93@gmail.com>
2020-08-22 17:46:01 -0400

    basic api endpoints

---
+++ meeting-insights/app.py
@@ -0,0 +1,38 @@
+import os
+import uuid
+
+from flask import Flask, url_for, request, jsonify
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+# from transcription import generate_transcript
+
+app = Flask(__name__)
+CORS(app)
+UPLOAD_DIRECTORY = './file_uploads/'
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify(status='ok')
+
+@app.route('/meeting/<meeting_id>', methods=['GET'])
+def meetings(meeting_id):
+    return jsonify(data='Showing meeting ID={}'.format(meeting_id))
+
+@app.route('/upload', methods=['POST'])
+def upload_new_meeting():
+    meeting_name = request.form.get("meeting_name")
+    meeting_date = request.form.get("date")
+    meeting_id = uuid.uuid4()
+
+    # get file
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(UPLOAD_DIRECTORY, filename)
+    file.save(filepath)
+    return jsonify(meeting_id=meeting_id, status='uploaded')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)
