e1d755285850467468b42b93a4805962490839cb
safeerahzayneb <39444082+safeerahzayneb@users.noreply.github.com>
2020-08-23 02:01:55 -0400

    integration changes

--- server/transcription_analytics.py
+++ server/transcription_analytics.py
@@ -2,7 +2,6 @@ from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 from textanalytics_azure import *
 from transcription import *
-import re
 
 key = "66809a9c3def40138dcf988dd9b13db5"
 endpoint = "https://textkeywords.cognitiveservices.azure.com/"
@@ -15,13 +14,15 @@ def authenticate_client():
     return text_analytics_client
 
 
+#def meeting_analytics();
+
 #client authentication
 client = authenticate_client()
 
 #speech to text transcription
 content = {}
 # Test Data
-vid_file_path='./test/sample2.mp4'
+vid_file_path='./sample2.mp4'
 file_name='test.wav'
 bucket_name='meeting_insights'
 
@@ -32,23 +33,16 @@ content = sample_recognize(gs_uri, content)
 #transcription analytics (azure API)
 text_dict = content
 text = ""
+output = ""
 for speaker in text_dict:
     for word in text_dict[speaker]:
         text += word + " "
 
 meeting_content = [text]
-key_phrases = key_phrase_extraction(client, meeting_content)
-entity_recognition = entity_recognition_example(client, meeting_content)
-entity_linking = entity_linking_example(client, meeting_content)
-sentiment_analysis = sentiment_analysis_example(client, meeting_content)
+key_phrases_out = key_phrase_extraction(client, meeting_content)
+entity_recog_out = entity_recognition(client, meeting_content)
+entity_linking_out = entity_linking(client, meeting_content)
+sentiment_analysis_out = sentiment_analysis(client, meeting_content)
+
 open("meeting_text.txt", "w").write(text)
 
-bullet_points = re.split("[,.]", text)
-text_summary = "Summary:\n"
-for line in bullet_points:
-    text_summary += "- " + line + "\n"
-
-print("key phrases", key_phrases)
-print("entities", entity_recognition)
-print("entities links", entity_linking)
-print("sentiment", sentiment_analysis)
