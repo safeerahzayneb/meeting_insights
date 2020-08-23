cde1bad1039039aa3d8052086dc567f4aeaae976
safeerahzayneb <39444082+safeerahzayneb@users.noreply.github.com>
2020-08-23 02:30:21 -0400

    created method to extract all information

--- server/transcription_analytics.py
+++ server/transcription_analytics.py
@@ -14,37 +14,39 @@ def authenticate_client():
     return text_analytics_client
 
 
-#def meeting_analytics();
+def meeting_analytics(UPLOAD_DIRECTORY):
 
-#client authentication
-client = authenticate_client()
+    #client authentication
+    client = authenticate_client()
 
-#speech to text transcription
-content = {}
-# Test Data
-vid_file_path='./sample2.mp4'
-file_name='test.wav'
-bucket_name='meeting_insights'
+    #speech to text transcription
+    content = {}
+    # Test Data
+    #vid_file_path='./sample2.mp4'
+    vid_file_path=UPLOAD_DIRECTORY +'/sample2.mp4'
+    file_name='test.wav'
+    bucket_name='meeting_insights'
 
-convert_mp4_to_wav(vid_file_path)
-gs_uri = upload_to_gs(file_name, bucket_name)
-content = sample_recognize(gs_uri, content)
+    convert_mp4_to_wav(vid_file_path)
+    gs_uri = upload_to_gs(file_name, bucket_name)
+    content = sample_recognize(gs_uri, content)
 
-#transcription analytics (azure API)
-text_dict = content
-text = ""
-output = ""
-for speaker in text_dict:
-    for word in text_dict[speaker]:
-        text += word + " "
+    #transcription analytics (azure API)
+    text_dict = content
+    text = ""
+    output = ""
+    for speaker in text_dict:
+        for word in text_dict[speaker]:
+            text += word + " "
 
-meeting_content = [text]
-key_phrases_out = key_phrase_extraction(client, meeting_content)
-entity_recog_out = entity_recognition(client, meeting_content)
-entity_linking_out = entity_linking(client, meeting_content)
-sentiment_analysis_out = sentiment_analysis(client, meeting_content)
+    meeting_content = [text]
+    key_phrases_out = key_phrase_extraction(client, meeting_content)
+    entity_recog_out = entity_recognition(client, meeting_content)
+    entity_linking_out = entity_linking(client, meeting_content)
+    sentiment_analysis_out = sentiment_analysis(client, meeting_content)
 
-output = key_phrases_out + "\n" + entity_recog_out + "\n" + entity_linking_out + "\n" + sentiment_analysis_out + "\n"
-print(output)
-open("meeting_text.txt", "w").write(text)
+    output = key_phrases_out + "\n" + entity_recog_out + "\n" + entity_linking_out + "\n" + sentiment_analysis_out + "\n"
+    #print(output)
+    open("meeting_text.txt", "w").write(text)
+    return output
 
