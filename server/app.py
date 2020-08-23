53452daf1270eecdba6b456c47bee31cea3f4199
Jennifer Chen <jenn.chenn93@gmail.com>
2020-08-23 06:06:04 -0400

    add sqlite

--- server/app.py
+++ server/app.py
@@ -1,15 +1,18 @@
 import os
 import uuid
+import json
+import datetime 
 
 from flask import Flask, url_for, request, jsonify
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from transcription_analytics import Analytics
+from db import add_data, get_data, deel
 # from transcription import generate_transcript
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
-UPLOAD_DIRECTORY = 'server/file_uploads/'
+UPLOAD_DIRECTORY = '/server/file_uploads/'
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
@@ -20,7 +23,11 @@ def health_check():
 
 @app.route('/meeting/<meeting_id>', methods=['GET'])
 def meetings(meeting_id):
-    return jsonify(data='Showing meeting ID={}'.format(meeting_id))
+    # deel()
+    data = get_data()
+    resp = jsonify(json.loads(data[0]))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 @app.route('/upload', methods=['POST'])
 def upload_new_meeting():
@@ -34,13 +41,17 @@ def upload_new_meeting():
     filepath = os.path.join(UPLOAD_DIRECTORY, filename)
     file.save(filepath)
     summary, key_phrases, entity_recog, entity_linking, sentiment_analysis = Analytics(filepath).meeting_analytics()
-    resp = jsonify(meeting_id=meeting_id,
-                   status='uploaded',
-                   summary=summary,
-                   key_phrases=key_phrases,
-                   entity_recog=entity_recog,
-                   entity_linking=entity_linking,
-                   sentiment_analysis=sentiment_analysis)
+    resp_dict = {
+        "meeting_id": str(meeting_id),
+        "date": str(datetime.datetime.now()),
+        "summary": summary,
+        "key_phrases": key_phrases,
+        "entity_recog": entity_recog,
+        "entity_linking": entity_linking,
+        "sentiment_analysis": sentiment_analysis
+    }
+    add_data(str(meeting_id), json.dumps(resp_dict))
+    resp = jsonify(resp_dict)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
